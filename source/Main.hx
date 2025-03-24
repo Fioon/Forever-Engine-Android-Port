18c3fb5ea14c188527790077e06198a104d3df7f
Fioon <135500399+Fioon@users.noreply.github.com>
2025-03-24 12:14:56 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -123,6 +123,9 @@ class Main extends Sprite
 		zoom = 1;
 		#end
 
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK]; 
+		#end
 		//Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 
 		// here we set up the base game
